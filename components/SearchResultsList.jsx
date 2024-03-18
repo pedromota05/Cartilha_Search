cc52af9655e09d52958644297c98f7bd3b93b153
Pedro Mota <94080428+pedromota05@users.noreply.github.com>
2024-03-18 10:14:17 -0400

    Update SearchResultsList.jsx

--- components/SearchResultsList.jsx
+++ components/SearchResultsList.jsx
@@ -1,14 +1,18 @@
 import Link from 'next/link';
 import { SearchResult } from "./SearchResult";
 
-export const SearchResultsList = ({ results }) => {
+export const SearchResultsList = ({ results, handleCloseResults  }) => {
   const mappedResults = results.map(item => ({
     ...item,
     chapterId: item.id // Supondo que o id seja equivalente ao chapterId
   }));
 
+  const handleResultClick = () => {
+    handleCloseResults();
+  };
+
   return (
-    <div className="results-list">
+    <div className="results-list" onClick={handleResultClick}>
       {mappedResults.map((result, id) => (
         <Link className='result-link' href={`/edicao-completa?activeChapter=${result.chapterId}#capitulo_${result.chapterId}`} key={id} passHref>
           <SearchResult result={result} />
